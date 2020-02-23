08874554876f88eace213b320022231601f81d11
Marine Mekhakyan <marinemekhakyan@gmail.com>
2020-02-22 19:13:21 -0800

    adding routes to pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Register from './pages/register/Register';
+import Login from './pages/login/Login';
+import Secure from './pages/Secure';
+
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className='flexbox'>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/secure' component={Secure} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
