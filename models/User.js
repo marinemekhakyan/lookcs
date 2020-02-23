de4aca9bc313f6a92766b7ed54fe72261a22d7f1
Marine Mekhakyan <marinemekhakyan@gmail.com>
2020-02-23 13:57:06 -0800

    adding user schema

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    hash: {
+        type: String,
+        required: true
+    } 
+    // dob: {
+    //     type: Date,
+    //     required: true
+    // }
+})
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
